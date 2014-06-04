92955263fcaac6239496c40a5fe505a1cd2c360d
Pavlo Romanenko <Pavlo_Romanenko@epam.com>
2014-06-04 14:40:28 +0300

    AOP stuff removed

--- src/main/java/com/gigaspaces/monitoring/metrics_reporter/CollectPeriodicAverageMetricsTask.java
+++ src/main/java/com/gigaspaces/monitoring/metrics_reporter/CollectPeriodicAverageMetricsTask.java
@@ -1,12 +1,10 @@
 package com.gigaspaces.monitoring.metrics_reporter;
 
 import com.gigaspaces.monitoring.metrics_source.adminapi.AdminAPIMonitor;
-import com.gigaspaces.monitoring.metrics_source.space_proxy.SpaceProxyCounter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Required;
 
-import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
@@ -16,8 +14,6 @@ public class CollectPeriodicAverageMetricsTask {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
-    private SpaceProxyCounter spaceProxyCounter;
-
     private AdminAPIMonitor adminMonitor;
 
     private Integer collectionPeriod;
@@ -26,20 +22,16 @@ public class CollectPeriodicAverageMetricsTask {
 
     private void collectMetrics(){
         logger.info("Collect timed moving average metrics");
-        logger.info("PERIODIC_METRICS = " + getMetrics(spaceProxyCounter.refreshTimedMetrics()));
+        logger.info("PERIODIC_METRICS = " + getMetrics());
     }
 
-    public String getMetrics(SpaceProxyCounter.PeriodicMetricsCounter periodicMetricsCounter){
+    public String getMetrics(){
 //        Memory Consumed / Have already (Admin API)
 //        Number of Puts / Have already (Admin API) ~ puts per ???
 //        Number of Gets / Have already (Admin API) ~ per ???
 //        Number of Removes / Have already (Admin API) ~ takes per ???
 //        Latency / Have already - latency per get (PI) - needs to be exposed
-        return  formatMetrics("cacheNum", adminMonitor.getObjectsCount()) + formatMetrics("cacheMemUsed", adminMonitor.getMemoryUsed())
-                + formatMetrics("cachePutCount", periodicMetricsCounter.getWriteCounter()) +
-                formatMetrics("cacheGetCount", periodicMetricsCounter.getReadCounter()) + formatMetrics("cacheRemoveCount", periodicMetricsCounter.getTakeCounter()) +
-                formatMetrics("cacheTransCount", periodicMetricsCounter.getTotalOperationsCount() / (collectionPeriod / 1000)) +
-                formatMetrics("cacheLatencyCount", periodicMetricsCounter.getAverageReadTime());
+        return  formatMetrics("cacheNum", adminMonitor.getObjectsCount()) + formatMetrics("cacheMemUsed", adminMonitor.getMemoryUsed());
     }
 
     private String formatMetrics(String metricType, Number metricValue){
@@ -61,10 +53,6 @@ public class CollectPeriodicAverageMetricsTask {
         this.adminMonitor = adminMonitor;
     }
 
-    @Required
-    public void setSpaceProxyCounter(SpaceProxyCounter spaceProxyCounter) {
-        this.spaceProxyCounter = spaceProxyCounter;
-    }
 
     public void setCollectionPeriod(Integer collectionPeriod) {
         this.collectionPeriod = collectionPeriod;
