36f4c92eeec10ee6627d3992e2b0f156b37b8743
Jason Nerothin <jasonnerothin@gmail.com>
2014-08-14 07:43:59 -0400

    Adding printout of all new enums.

---
+++ src/main/java/com/gigaspaces/sbp/metrics/PrintAll.java
@@ -0,0 +1,36 @@
+package com.gigaspaces.sbp.metrics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jason
+ * Date: 8/7/14
+ * Time: 9:43 AM
+ * Provides...
+ */
+public class PrintAll {
+
+    public static void main(String[] args){
+
+        List<NamedMetric> metrics = new ArrayList<>();
+        metrics.addAll(Arrays.asList(GigaSpacesActivity.values()));
+        metrics.addAll(Arrays.asList(GigaSpacesClusterInfo.values()));
+        metrics.addAll(Arrays.asList(GsMirrorInfo.values()));
+        metrics.addAll(Arrays.asList(JvmInfo.values()));
+        metrics.addAll(Arrays.asList(Memory.values()));
+        metrics.addAll(Arrays.asList(OperatingSystemInfo.values()));
+
+
+        StringBuilder buff = new StringBuilder();
+        for( NamedMetric metric : metrics ){
+            buff.append(metric.displayName()).append("\n");
+        }
+
+        System.out.println(buff.toString());
+
+    }
+
+}
