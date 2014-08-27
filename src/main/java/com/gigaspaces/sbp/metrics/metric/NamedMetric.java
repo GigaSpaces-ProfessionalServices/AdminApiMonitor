783af403e5cfcf854834eefb956bdc132b68dc2d
Pavlo Romanenko <Pavlo_Romanenko@epam.com>
2014-08-27 12:42:15 +0300

    JMX beans reusage

--- src/main/java/com/gigaspaces/sbp/metrics/metric/NamedMetric.java
+++ src/main/java/com/gigaspaces/sbp/metrics/metric/NamedMetric.java
@@ -1,25 +1,14 @@
 package com.gigaspaces.sbp.metrics.metric;
 
-import com.gigaspaces.cluster.replication.async.mirror.MirrorStatistics;
 import com.gigaspaces.sbp.metrics.visitor.StatsVisitor;
-import com.j_spaces.core.filters.ReplicationStatistics;
-import org.openspaces.admin.gsc.GridServiceContainer;
-import org.openspaces.admin.space.SpaceInstance;
-import org.openspaces.admin.space.SpaceInstanceStatistics;
 import org.openspaces.admin.vm.VirtualMachineDetails;
-import org.openspaces.admin.vm.VirtualMachineStatistics;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.management.*;
-import javax.management.openmbean.CompositeDataSupport;
 import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 import java.io.IOException;
-import java.util.Date;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -52,11 +41,21 @@ class JmxUtils{
 
     static final String OS_SEARCH_STRING = "java.lang:type=OperatingSystem";
 
+    static final Map<String,MBeanServerConnection> connections = new HashMap<>();
+
     MBeanServerConnection mbeanServer(VirtualMachineDetails details, String queryString) throws IOException, MalformedObjectNameException {
-        JMXServiceURL jmxUrl = new JMXServiceURL(details.getJmxUrl());
-        JMXConnector connection = JMXConnectorFactory.newJMXConnector(jmxUrl, null);
-        connection.connect(null);
-        return connection.getMBeanServerConnection();
+        String jmxUrl1 = details.getJmxUrl();
+        if (!connections.containsKey(jmxUrl1)){
+            JMXServiceURL jmxUrl = new JMXServiceURL(jmxUrl1);
+            JMXConnector connection = JMXConnectorFactory.connect(jmxUrl);
+            connection.connect(null);
+            MBeanServerConnection mBeanServerConnection = connection.getMBeanServerConnection();
+            connections.put(jmxUrl1, mBeanServerConnection);
+            return mBeanServerConnection;
+        }   else {
+            return connections.get(jmxUrl1);
+        }
+
     }
 
 }
