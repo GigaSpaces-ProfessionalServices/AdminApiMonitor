749f5a203aa43c83ab189db2662287bdc53ef846
Pavlo Romanenko <Pavlo_Romanenko@epam.com>
2014-08-29 13:25:00 +0300

    SpaceMode for every SpaceInstance

--- src/main/java/com/gigaspaces/sbp/metrics/metric/GigaSpacesClusterInfo.java
+++ src/main/java/com/gigaspaces/sbp/metrics/metric/GigaSpacesClusterInfo.java
@@ -2,15 +2,16 @@ package com.gigaspaces.sbp.metrics.metric;
 
 import com.gigaspaces.sbp.metrics.FullMetric;
 import com.gigaspaces.sbp.metrics.visitor.StatsVisitor;
+import org.openspaces.admin.pu.ProcessingUnitInstance;
 import org.openspaces.admin.space.SpaceInstance;
 
 public enum GigaSpacesClusterInfo implements NamedMetric {
 
     SPACE_MODE("space_mode"){
         public void accept(StatsVisitor statsVisitor){
-            SpaceInstance spaceInstance = statsVisitor.spaceInstance().get(0);
-            if( spaceInstance != null ){
-                statsVisitor.saveStat(new FullMetric.FullMetricBuilder().metric(this).metricValue(spaceInstance.getMode().name()).create());
+            for (SpaceInstance spaceInstance : statsVisitor.spaceInstance()){
+                statsVisitor.saveStat(new FullMetric.FullMetricBuilder().metric(this).metricValue(spaceInstance.getMode().name()).
+                        spaceInstanceID(spaceInstance.getSpaceInstanceName()).create());
             }
         }
     }
