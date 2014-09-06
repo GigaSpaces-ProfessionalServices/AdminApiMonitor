587ffcdaaf9dcc349b5f18d0dc749d53fcf724cc
Jason Nerothin <jasonnerothin@gmail.com>
2014-09-05 22:05:11 -0500

    printing hostname to output file

--- src/main/bash/gsc_cpu_monitor.sh
+++ src/main/bash/gsc_cpu_monitor.sh
@@ -11,6 +11,7 @@ PERIOD=30
 #   CPU is the percentage of cpu on that processor at time that 'ps' was run
 #
 
+echo "CPU stats collected on $(hostname)"
 echo "timestamp, PID, PSR, %CPU"
 while true; do
    for i in `ps aux | grep GSC | grep -v grep| grep -v ' start '| awk '{print $2}'`; do cur_date=`date +"%m-%d-%Y-%T"`; PID=${i}; ps -p $i -L -o pid,psr,pcpu | grep -v PID | awk -vcur_date=${cur_date} -vPID=${PID} '{arr[$2]+=$3} END {for (i in arr) {print cur_date,PID,i,arr[i]}}';done
