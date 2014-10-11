316bedec7ab775b21255fad3f44bcc35360328ee
unknown <Pavlo_Romanenko@EPUAKHAL0013.kyiv.epam.com>
2014-10-11 22:57:49 +0300

    install.bat fixed

--- install.bat
+++ install.bat
@@ -30,8 +30,8 @@ ECHO %INTERVAL%
 SET /p DELAY="Please enter statistics collection delay in milliseconds (format: 10000) : " %=%
 ECHO %DELAY%
 
-SET /p ALPHA="Please enter EMA alpha (format: 0.8) : " %=%
-ECHO %ALPHA%
+SET /p CONST="Please enter EMA alpha (format: 0.8) : " %=%
+ECHO %CONST%
 
 ::    Using read, ask for inputs to update log file output path and logging type (choose from variants)
 SET /p LOGFILE="Please enter path to log file : " %=%
@@ -47,20 +47,26 @@ ECHO %LOGGING_TYPE%
 
 IF %LOGGING_TYPE% EQU 1 ( 
 SET LOGGING_CONFIG=logback-stdout.xml
+SET CSV=
 ) ELSE IF %LOGGING_TYPE% EQU 2 ( 
 SET LOGGING_CONFIG=logback-default.xml 
+SET CSV=
 ) ELSE IF %LOGGING_TYPE% EQU 3 ( 
 SET LOGGING_CONFIG=logback-csv.xml 
 SET CSV=-c
 ) ELSE IF %LOGGING_TYPE% EQU 3 ( 
-SET LOGGING_CONFIG=logback-email.xml 
+SET LOGGING_CONFIG=logback-email.xml
+SET CSV= 
 ) ELSE (
 ECHO "Incorrect choice" 
 goto :choice
 )
 
-ECHO %LOGGING_CONFIG%
+IF %LOGGING_TYPE% EQU 3 (  
+SET CSV=-c)
 
+ECHO %LOGGING_CONFIG%
+ECHO CSV=%CSV%
 
 SET /p INSTALL_DIR="Please enter path to installation directory:" %=%
 ECHO %INSTALL_DIR%
@@ -73,21 +79,41 @@ xcopy target\AdminApiMonitor.jar %INSTALL_DIR%
 xcopy target\logback\%LOGGING_CONFIG% %INSTALL_DIR% 
 ::unzip target/AdminApiMonitor-ConfigurationFiles.zip -d $installDir/config
 cd %INSTALL_DIR%
+
+ECHO %CONST%
+
 echo spaceMonitor.spaceName=%SPACES%>admin-api.properties
 echo spaceMonitor.locators=%LOCATORS%>>admin-api.properties
 echo spaceMonitor.secured=%SECURED%>>admin-api.properties
 echo spaceMonitor.adminUser=%NAME%>>admin-api.properties
 echo spaceMonitor.adminPassword=%PASSWORD%>>admin-api.properties
 echo spaceMonitor.lookupGroups=%LOOKUP_GROUPS%>>admin-api.properties
-echo stat.sample.alpha=%ALPHA%>>admin-api.properties
 echo stat.periodic.sample.interval=%INTERVAL%>>admin-api.properties
 echo stat.periodic.sample.delay=%DELAY%>>admin-api.properties
+echo stat.sample.alpha=%CONST% >>admin-api.properties
 echo alerts.config=alerts-config.xml>>admin-api.properties
 echo alerts.email.reporting=false>>admin-api.properties
 
-::sudo sed -i "s#admin-api.log#$logfile#g" $LOGGING_CONFIG
+::SED EQUIVALENT START
+@echo off
+setlocal enabledelayedexpansion
+set INTEXTFILE=%LOGGING_CONFIG%
+set OUTTEXTFILE=test_out.txt
+set SEARCHTEXT=admin-api.log
+set REPLACETEXT=%LOGFILE%
+set OUTPUTLINE=
 
-ECHO java -Dproperties=%INSTALL_DIR%\admin-api.properties -Dlog.file=%LOGFILE% -Dlogback.configurationFile=%INSTALL_DIR%\%LOGGING_CONFIG%  -jar AdminApiMonitor.jar %CSV%
+for /f "tokens=1,* delims=¶" %%A in ( '"type %INTEXTFILE%"') do (
+SET string=%%A
+SET modified=!string:%SEARCHTEXT%=%REPLACETEXT%!
+
+echo !modified! >> %OUTTEXTFILE%
+)
+del %INTEXTFILE%
+rename %OUTTEXTFILE% %INTEXTFILE%
+::SED EQUIVALENT END
+
+::ECHO java -Dproperties=%INSTALL_DIR%\admin-api.properties -Dlog.file=%LOGFILE% -Dlogback.configurationFile=%INSTALL_DIR%\%LOGGING_CONFIG%  -jar AdminApiMonitor.jar %CSV%
 java -Dproperties=%INSTALL_DIR%\admin-api.properties -Dlog.file=%LOGFILE% -Dlogback.configurationFile=%INSTALL_DIR%\%LOGGING_CONFIG%  -jar AdminApiMonitor.jar %CSV%
 ::chmod +x run.sh
 ::./run.sh
