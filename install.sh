52ae5c0bb0eb6bcb425ac79be1235cf621d30bf6
Jason Nerothin <jasonnerothin@gmail.com>
2014-10-11 23:45:29 -0500

    adding default values and fixing a minor thing or two

--- install.sh
+++ install.sh
@@ -1,70 +1,93 @@
 #!/bin/bash
 
+DEFAULT_LOOKUPLOCATORS="localhost:4174"
+DEFAULT_SPACENAME="mySpace"
+DEFAULT_COLLECTIONINTERVAL=60000
+DEFAULT_COLLECTIONDELAY=10000
+DEFAULT_EMA_ALPHA=0.8
+DEFAULT_LOGFILE="/tmp/AdminApiMonitor.log"
+DEFAULT_INSTALLDIR="/tmp/"
+RUN_SCRIPT="run-adminapimonitor.sh"
+
+CWD=$(pwd)
+
 # check root
 
 if [[ $EUID -ne 0 ]]; then
-    echo "This script should not be run using sudo or as the root user"
+    echo "This script should be run using sudo or as the root user"
     exit 1
 fi
 
+# Using read, ask for inputs to populate properties file.
 
-#    Using read, ask for inputs to populate properties file.
+read -p "Please enter space names (format: space1,space2,mirror-service) [${DEFAULT_SPACENAME}] : " spacename
+if [ "${spacename}" == "" ]; then
+    spacename="${DEFAULT_SPACENAME}"
+fi
+echo ${spacename}
 
+read -p "Please enter lookup locators (format: 10.0.2.15:4174) [${DEFAULT_LOOKUPLOCATORS}] : " locators
+if [ "${locators}" == "" ]; then
+    locators="${DEFAULT_LOOKUPLOCATORS}"
+fi
+echo ${locators}
 
-read -p "Please enter space names (format: space1,space2,mirror-service) : " spacename
-echo $spacename
-
-read -p "Please enter locators (format: 10.0.2.15:4174,10.0.2.16:4174) : " locators
-echo $locators
-
-read -p "Please enter is space secured (y/n) : " RESP
-if [ "$RESP" = "y" ]; then
+read -p "Is the space secured? (y/n) [n]: " RESP
+if [ "$RESP" == "y" ]; then
   secured="true"
-  echo "Secured space" 
-  read -p "Please enter admin's username : " username  
+  read -p "Please enter admin's username : " username
   read -p "Please enter admin's password : " password
 else
-  secured=false
-  echo "space is not secured"
+  secured="false"
 fi
+echo "${secured}"
 
-read -p "Please enter lookup groups (format: )(optional) : " lookupGroups
-echo $lookupGroups
+read -p "Please enter (optional) lookup groups (format: someGroup) ['']: " lookupGroups
+echo ${lookupGroups}
 
-read -p "Please enter statistics collection insterval in milliseconds (format: 10000) : " interval
-echo $interval
+read -p "Please enter metric collection interval in milliseconds (format: 1000) [${DEFAULT_COLLECTIONINTERVAL}]: " interval
+if [ "${interval}" == "" ]; then
+    interval="${DEFAULT_COLLECTIONINTERVAL}"
+fi
+echo ${interval}
 
-read -p "Please enter statistics collection delay in milliseconds (format: 3000) : " delay
-echo $delay
-
-read -p "Please enter EMA alpha (format: 0.8) : " alpha
-echo $alpha
-
+read -p "Please enter metric delay in milliseconds (format: 5000) [${DEFAULT_COLLECTIONDELAY}]: " delay
+if [ "${delay}" == "" ]; then
+    delay="${DEFAULT_COLLECTIONDELAY}"
+fi
+echo ${delay}
 
 #    Using read, ask for inputs to update log file output path and logging type (choose from variants)
-read -p "Please enter path to log file : " logfile
-echo $logfile
+read -p "Please enter path to log file [${DEFAULT_LOGFILE}]: " logfile
+if [ "${logfile}" == "" ]; then
+    logfile="${DEFAULT_LOGFILE}"
+fi
+echo ${logfile}
+if [ ! -f ${logfile} ]; then
+    echo "mkdir -p dirname ${logfile}"
+    mkdir -p dirname ${logfile}
+fi
 
 select result in Console Standard CSV Email
 do
-    echo $result
-    if [ "$result" == "Console" ]
+    echo ${result}
+    if [ "${result}" == "Console" ]
     then
       LOGGING_CONFIG="logback-stdout.xml"
-    elif [ "$result" == "CSV" ]
+    elif [ "${result}" == "CSV" ]
     then
       LOGGING_CONFIG="logback-csv.xml"
       CSV="-c"
-    elif [ "$result" == "Standard" ]
+    elif [ "${result}" == "Standard" ]
     then
       LOGGING_CONFIG="logback-default.xml"
-    elif [ "$result" == "Email" ]
+    elif [ "${result}" == "Email" ]
     then
-      LOGGING_CONFIG="logback-email.xml"     
+      LOGGING_CONFIG="logback-email.xml"
     else
       echo "Please choose appropriate type of logging"
     fi
-    echo $LOGGING_CONFIG
+#    echo ${LOGGING_CONFIG}
     break
 done
 
@@ -72,36 +95,72 @@ done
 #    dump the scripts directory output into the configuration directory
 #    Using read, ask for installation directory
 
-read -p "Please enter path to installation directory: " installDir
-echo $installDir
+read -p "Please enter path to installation directory [${DEFAULT_INSTALLDIR}]: " installDir
+if [ "${installDir}" == "" ]; then
+  installDir="${DEFAULT_INSTALLDIR}"
+fi
+echo ${installDir}
 
-mvn clean install
+# CONFIRM WE WANT TO PROCEED
 
-mkdir $installDir/AdminApiMonitor
-installDir=$installDir/AdminApiMonitor
-cp target/AdminApiMonitor.jar $installDir
-cp target/logback/$LOGGING_CONFIG $installDir 
-unzip target/AdminApiMonitor-ConfigurationFiles.zip -d $installDir/config
-cd $installDir
-echo "spaceMonitor.spaceName=$spacename" > 'admin-api.properties'
-echo "spaceMonitor.locators=$locators" >> 'admin-api.properties'
-echo "spaceMonitor.secured=$secured" >> 'admin-api.properties'
-echo "spaceMonitor.adminUser=$username" >> 'admin-api.properties'
-echo "spaceMonitor.adminPassword=$password" >> 'admin-api.properties'
-echo "spaceMonitor.lookupGroups=$lookupGroups" >> 'admin-api.properties'
-echo "stat.sample.alpha=$alpha" >> 'admin-api.properties'
-echo "stat.periodic.sample.interval=$interval" >> 'admin-api.properties'
-echo "stat.periodic.sample.delay=$delay" >> 'admin-api.properties'
+echo "###################"
+echo "## CONFIGURATION ##"
+echo "###################"
+echo "space(s): ${spacename}"
+echo "locator(s): ${locators}"
+echo "lookup groups: '${lookupGroups}"
+echo "secured: ${secured}"
+echo "username: ${username}"
+echo "password: *******"
+echo ""
+echo "collection interval (ms): ${interval}"
+echo "collection delay: ${delay}"
+echo ""
+echo "logfile: ${logfile}"
+echo "install directory: %{installDir}"
+echo "output format: ${result}"
+echo ""
+read -p "Proceed? (y/n)[n]" proceed
+if [ "${proceed}" != "y" ]; then
+    echo "goodbye"
+    exit 666
+fi
+
+mvn clean install || echo "XXXXXXX > MAVEN BUILD FAILURE < XXXXXXXX" && exit 1
+
+mkdir -p ${installDir}/AdminApiMonitor
+installDir=${installDir}/AdminApiMonitor
+cp target/AdminApiMonitor.jar ${installDir}
+cp target/logback/${LOGGING_CONFIG} ${installDir}
+unzip target/AdminApiMonitor-ConfigurationFiles.zip -d ${installDir}/config
+cd ${installDir}
+
+echo "spaceMonitor.spaceName=${spacename}" > 'admin-api.properties'
+echo "spaceMonitor.locators=${locators}" >> 'admin-api.properties'
+echo "spaceMonitor.secured=${secured}" >> 'admin-api.properties'
+echo "spaceMonitor.adminUser=${username}" >> 'admin-api.properties'
+echo "spaceMonitor.adminPassword=${password}" >> 'admin-api.properties'
+echo "spaceMonitor.lookupGroups=${lookupGroups}" >> 'admin-api.properties'
+echo "stat.sample.alpha=${DEFAULT_EMA_ALPHA}" >> 'admin-api.properties'
+echo "stat.periodic.sample.interval=${interval}" >> 'admin-api.properties'
+echo "stat.periodic.sample.delay=${delay}" >> 'admin-api.properties'
 echo "alerts.config=alerts-config.xml" >> 'admin-api.properties'
 echo "alerts.email.reporting=false" >> 'admin-api.properties'
 
-echo "sudo sed -i "s/admin-api.log/$logfile/g" $LOGGING_CONFIG"
-sudo sed -i "s#admin-api.log#$logfile#g" $LOGGING_CONFIG
+echo "sudo sed -i "s/admin-api\.log/${logfile}/g" ${LOGGING_CONFIG}"
+sudo sed -i "s#admin-api\.log#$logfile#g" ${LOGGING_CONFIG}
 
-echo "java -Dproperties=$installDir/admin-api.properties -Dlog.file=$logfile -Dlogback.configurationFile=$installDir/$LOGGING_CONFIG  -jar AdminApiMonitor.jar $CSV" > run.sh
-#java -Dproperties=$installDir/admin-api.properties -Dlog.file=$logfile -Dlogback.configurationFile=$installDir/$LOGGING_CONFIG  -jar AdminApiMonitor.jar $CSV
-chmod +x run.sh
-./run.sh
+if [ -f ${RUN_SCRIPT} ]; then
+    rm -f ${RUN_SCRIPT}
+fi
 
+SECURED_FLAG=""
+if [ "${secured}" == "true" ]; then
+    SECURED_FLAG = "-s"
+fi
 
+echo "java -Dproperties=${installDir}/admin-api.properties -Dlog.file=${logfile} -Dlogback.configurationFile=${installDir}/${LOGGING_CONFIG} -jar AdminApiMonitor.jar ${SECURED_FLAG} ${CSV}" > ${RUN_SCRIPT}
+chmod +x ${RUN_SCRIPT}
+./${RUN_SCRIPT}
 
+cd ${CWD} # in case of run script failure
