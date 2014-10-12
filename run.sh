7b599fee720de6f120475c035a4c0ef4b3db8600
Jason Nerothin <jasonnerothin@gmail.com>
2014-10-11 23:59:37 -0500

    cleaning up mvn complaint

---
+++ run.sh
@@ -0,0 +1,1 @@
+java -Dproperties=/tmp/monitor/AdminApiMonitor/admin-api.properties -Dlog.file=/tmp/my.csv -Dlogback.configurationFile=/tmp/monitor/AdminApiMonitor/logback-csv.xml  -jar AdminApiMonitor.jar -c
---
+++ src/main/bash/belk_start_monitor_csv.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/belk/jdk1.7.0_21/bin/java -Dproperties=/belk/admin_api.properties -Dlogback.configurationFile=/belk/belk-csv-logback.xml -jar /belk/AdminApiMonitor.jar -s -c &
