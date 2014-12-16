5e5921bb4ab2b055a648182677280284f7cb1c3e
Jason Nerothin <jasonnerothin@gmail.com>
2014-12-16 12:27:00 -0600

    Assertion messages I use way too frequently.

--- src/main/java/com/gigaspaces/sbp/metrics/Constants.java
+++ src/main/java/com/gigaspaces/sbp/metrics/Constants.java
@@ -11,5 +11,7 @@ public interface Constants {
     String DATE_FORMAT = "dd-M-yyyy HH:mm:ss";
     String DEFAULT_FLAG_VALUE = Boolean.TRUE.toString();
     String LIST_ITEM_SEPARATOR = ",";
+    String THING_REQUIRED = "A %s is required.";
+    String THINGS_REQUIRED = "%s are required.";
 
 }
